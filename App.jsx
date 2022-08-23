39346768abaf002049fa9c58e1d867f4d726e2f7
Robert Romano <bromano1992@gmail.com>
2022-08-23 16:15:43 -0400

    Close and Delete buttons added on ViewImageScreen

--- App.jsx
+++ App.jsx
@@ -15,9 +15,10 @@ import {
 } from "react-native"
 import { useDimensions, useDeviceOrientation } from "@react-native-community/hooks"
 import WelcomeScreen from "./app/screens/WelcomeScreen"
+import ViewImageScreen from "./app/screens/ViewImageScreen"
 
 export default function App() {
-	return <WelcomeScreen />
+	return <ViewImageScreen />
 }
 
 const styles = StyleSheet.create({
