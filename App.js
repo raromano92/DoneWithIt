c1496c23d83d5c5e4415cbd242dab7e4c65577f6
Robert Romano <bromano1992@gmail.com>
2022-08-23 01:31:39 -0400

    Experimenting with diff react native properties

--- App.js
+++ App.js
@@ -1,26 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import {
+	StyleSheet,
+	Text,
+	TouchableWithoutFeedback,
+	TouchableOpacity,
+	TouchableHighlight,
+	TouchableNativeFeedback,
+	Image,
+	View,
+	SafeAreaView,
+} from "react-native"
 
 export default function App() {
+	console.log("HEYEYEYE")
+	const handlePress = () => console.log("Text pressed")
+
 	return (
-		<View style={styles.container}>
-			<Text style={styles.setFontSizeTwo}>Testing</Text>
+		<SafeAreaView style={styles.container}>
+			<Text numberOfLines={1} onPress={handlePress} style={styles.setFontSizeTwo}>
+				abcdefg
+			</Text>
+			<TouchableHighlight onPress={() => console.log("Tapped picture")}>
+				<Image
+					blurRadius={0}
+					fadeDuration={1000}
+					source={{
+						width: 200,
+						height: 300,
+						uri: "https://picsum.photos/id/237/200/300",
+					}}
+				/>
+			</TouchableHighlight>
+			<TouchableNativeFeedback>
+				<View style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}></View>
+			</TouchableNativeFeedback>
 			<StatusBar style="auto" />
-		</View>
-	);
+		</SafeAreaView>
+	)
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+		fontWeight: "bold",
 	},
+
 	setFontSizeOne: {
 		fontSize: 15, // Define font size here in Pixels
 	},
 	setFontSizeTwo: {
 		fontSize: 40, // Define font size here in Pixels
 	},
-});
+})
