87c6259b854602a4a241a15dceaa7083e0bff9c1
Robert Romano <bromano1992@gmail.com>
2022-08-24 02:33:14 -0400

    Listing details screen completed

--- App.jsx
+++ App.jsx
@@ -22,23 +22,10 @@ import TextStyle from "./app/components/TextStyle"
 import AppText from "./app/components/AppText"
 import AppButton from "./app/components/AppButton"
 import Card from "./app/components/Card"
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen"
 
 export default function App() {
-	return (
-		<View
-			style={{
-				backgroundColor: "#f8f4f4",
-				padding: 20,
-				paddingTop: 100,
-			}}
-		>
-			<Card
-				title="Red jacket for sale"
-				subTitle="$100"
-				image={require("./app/assets/jacket.jpg")}
-			/>
-		</View>
-	)
+	return <ListingDetailsScreen />
 
 	{
 		/* <AppButton
