7ec58036070af639d3792b26a67e5c6c80cee86c
Robert Romano <bromano1992@gmail.com>
2022-08-27 15:26:30 -0400

    Added api call for posting form data

--- app/api/listings.js
+++ app/api/listings.js
@@ -1,9 +1,27 @@
-import client from './client'
+import { date } from 'yup';
+import client from './client';
 
-const endpoint = '/listings'
+const endpoint = '/listings';
 
-const getListings = () => client.get(endpoint)
+const getListings = () => client.get(endpoint);
+
+const addListing = (listing) => {
+	new FormData();
+	data.append('title', listing.title);
+	data.append('price', listing.price);
+	data.append('categoryId', listing.category.value);
+	data.append('description', listing.value);
+
+	listing.images.forEach((image, index) =>
+		data.append('images', { name: 'image' + index, type: 'image/jpeg', uri: image })
+	);
+
+	if (listing.location) data.append('location', JSON.stringify(listing.location));
+
+	return client.post(endpoint, data);
+};
 
 export default {
+	addListing,
 	getListings,
-}
+};
