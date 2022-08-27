3b4c7b957603f0c88588948fd5de42dfcb3eaff1
Robert Romano <bromano1992@gmail.com>
2022-08-26 21:34:42 -0400

    Added AppNavigator and tab switching is functioning

--- app/hooks/useLocation.jsx
+++ app/hooks/useLocation.jsx
@@ -10,7 +10,7 @@ export default useLocation = () => {
 			if (!granted) return
 			const {
 				coords: { latitude, longitude },
-			} = await Location.getLastKnownPositionAsync()
+			} = await Location.getCurrentPositionAsync()
 			setLocation({ latitude, longitude })
 		} catch (error) {
 			console.log(error)
