88da5c6a2fcb5b9768474bc6f552e39ac2ce160b
Robert Romano <bromano1992@gmail.com>
2022-08-27 00:28:02 -0400

    Installed apisauce and setup boilerplate to talk to backend

---
+++ app/api/client.js
@@ -0,0 +1,11 @@
+import { create } from 'apisauce'
+
+const apiClient = create({
+	baseURL: 'http://10.0.0.18:9000/assets/',
+})
+
+apiClient.get('/listings').then((response) => {
+	if (!response.ok) {
+		response.problem
+	}
+})
--- app/screens/AccountScreen.jsx
+++ app/screens/AccountScreen.jsx
@@ -6,6 +6,7 @@ import Screen from '../components/Screen'
 import colors from '../config/colors'
 import Icon from '../components/Icon'
 import ListItemSeparator from '../components/lists/ListItemSeparator'
+import routes from '../navigation/routes'
 
 const menuItems = [
 	{
@@ -21,7 +22,7 @@ const menuItems = [
 			name: 'email',
 			backgroundColor: colors.secondary,
 		},
-		targetScreen: 'Messages',
+		targetScreen: routes.MESSSAGES,
 	},
 ]
 
