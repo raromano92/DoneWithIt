dc2dc7850bdc125d440a848e8d1e6aa3a4b1ac97
Robert Romano <bromano1992@gmail.com>
2022-09-03 16:51:07 -0400

    Added Caching file for getting, removing, storing items in cache

--- app/components/forms/AppFormField.jsx
+++ app/components/forms/AppFormField.jsx
@@ -1,26 +1,24 @@
-import React from "react"
-import { useFormikContext } from "formik"
+import React from 'react';
+import { useFormikContext } from 'formik';
 
-import AppTextInput from "../AppTextInput"
-import ErrorMessage from "./ErrorMessage"
+import AppTextInput from '../AppTextInput';
+import ErrorMessage from './ErrorMessage';
 
 function AppFormField({ name, width, ...otherProps }) {
-	const { setFieldTouched, handleChange, errors, touched } = useFormikContext()
+	const { setFieldTouched, setFieldValue, errors, touched, values } = useFormikContext();
 
 	return (
 		<>
 			<AppTextInput
 				onBlur={() => setFieldTouched(name)}
-				onChangeText={handleChange(name)}
+				onChangeText={(text) => setFieldValue(name, text)}
+				value={values[name]}
 				width={width}
 				{...otherProps}
 			/>
-			<ErrorMessage
-				error={errors[name]}
-				visible={touched[name]}
-			/>
+			<ErrorMessage error={errors[name]} visible={touched[name]} />
 		</>
-	)
+	);
 }
 
-export default AppFormField
+export default AppFormField;
