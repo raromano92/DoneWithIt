5bc8b648fa5edb81db8b2521689752c4cec6601d
Robert Romano <bromano1992@gmail.com>
2022-09-03 20:47:01 -0400

    Added in offline notice if user is offline while cached data displaying

--- App.jsx
+++ App.jsx
@@ -1,28 +1,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import NetInfo, { useNetInfo } from '@react-native-community/netinfo';
-import { AsyncStorage } from 'react-native';
 
 import AppNavigator from './app/navigation/AppNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
+import OfflineNotice from './app/components/OfflineNotice';
 
 export default function App() {
-	const demo = async () => {
-		try {
-			await AsyncStorage.setItem('person', JSON.stringify({ id: 1 }));
-			const value = await AsyncStorage.getItem('person');
-			const person = JSON.parse(value);
-			console.log(person);
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
-	demo();
-
 	return (
-		<NavigationContainer theme={navigationTheme}>
-			<AppNavigator />
-		</NavigationContainer>
+		<>
+			<NavigationContainer theme={navigationTheme}>
+				<AppNavigator />
+				<OfflineNotice />
+			</NavigationContainer>
+		</>
 	);
 }
