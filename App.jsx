42426db7470221cfbaacc897c5a32e1c943e047e
Robert Romano <bromano1992@gmail.com>
2022-09-04 16:40:33 -0400

    Confirmed user is held in authcontext and can now log in

--- App.jsx
+++ App.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import AppNavigator from './app/navigation/AppNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
 import OfflineNotice from './app/components/OfflineNotice';
 import AuthNavigator from './app/navigation/AuthNavigator';
+import AuthContext from './app/auth/context';
 
 export default function App() {
+	const [user, setUser] = useState();
+
 	return (
-		<>
+		<AuthContext.Provider value={{ user, setUser }}>
+			<OfflineNotice />
 			<NavigationContainer theme={navigationTheme}>
-				<OfflineNotice />
-				<AuthNavigator />
+				{user ? <AppNavigator /> : <AuthNavigator />}
 			</NavigationContainer>
-		</>
+		</AuthContext.Provider>
 	);
 }
