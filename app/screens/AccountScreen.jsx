9541b4f2db939692ebc5b80843d34973c81675b4
Robert Romano <bromano1992@gmail.com>
2022-09-05 20:40:57 -0400

    Created custom hook for auth code and refactored further

--- app/screens/AccountScreen.jsx
+++ app/screens/AccountScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { StyleSheet, View, FlatList } from 'react-native';
 
 import ListItem from '../components/lists/ListItem';
@@ -7,8 +7,7 @@ import colors from '../config/colors';
 import Icon from '../components/Icon';
 import ListItemSeparator from '../components/lists/ListItemSeparator';
 import routes from '../navigation/routes';
-import AuthContext from '../auth/context';
-import authStorage from '../auth/storage';
+import useAuth from '../auth/useAuth';
 
 const menuItems = [
 	{
@@ -29,12 +28,7 @@ const menuItems = [
 ];
 
 export default function AccountScreen({ navigation }) {
-	const { user, setUser } = useContext(AuthContext);
-
-	const handleLogOut = () => {
-		setUser(null);
-		authStorage.removeToken();
-	};
+	const { user, logOut } = useAuth();
 
 	return (
 		<Screen style={styles.screen}>
@@ -66,7 +60,7 @@ export default function AccountScreen({ navigation }) {
 			<ListItem
 				title='Log Out'
 				IconComponent={<Icon name='logout' backgroundColor='#ffe66d' />}
-				onPress={handleLogOut}
+				onPress={() => logOut()}
 			/>
 		</Screen>
 	);
--- app/screens/LoginScreen.jsx
+++ app/screens/LoginScreen.jsx
@@ -1,13 +1,10 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Image } from 'react-native';
 import * as Yup from 'yup';
-import jwtDecode from 'jwt-decode';
 
 import Screen from '../components/Screen';
 import { ErrorMessage, AppForm, AppFormField, SubmitButton } from '../components/forms';
 import authApi from '../api/auth';
-import authStorage from '../auth/storage';
-import AuthContext from '../auth/context';
 
 const validationSchema = Yup.object().shape({
 	email: Yup.string().required().email().label('Email'),
@@ -15,17 +12,14 @@ const validationSchema = Yup.object().shape({
 });
 
 function LoginScreen(props) {
-	const authContext = useContext(AuthContext);
+	const auth = useAuth();
 	const [loginFailed, setLoginFailed] = useState(false);
 
 	const handleSubmit = async ({ email, password }) => {
 		const result = await authApi.login(email, password);
 		if (!result.ok) return setLoginFailed(true);
 		setLoginFailed(false);
-		const user = jwtDecode(result.data);
-		authContext.setUser(user);
-		authStorage.storeToken(result.data);
-		console.log('testinmg');
+		auth.logIn(result.data);
 	};
 
 	return (
