85ed7f44b1b2eae1e5394e0555a960dcb13e14c5
Robert Romano <bromano1992@gmail.com>
2022-09-06 01:25:39 -0400

    Implementing push notifications, now able to obtain a token for it

--- app/navigation/AppNavigator.jsx
+++ app/navigation/AppNavigator.jsx
@@ -1,59 +1,76 @@
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { MaterialCommunityIcons } from '@expo/vector-icons'
+import React, { useEffect } from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
 
-import ListingEditScreen from '../screens/ListingEditScreen'
-import FeedNavigator from './FeedNavigator'
-import AccountNavigator from './AccountNavigator'
-import NewListingButton from './NewListingButton'
-import routes from './routes'
+import ListingEditScreen from '../screens/ListingEditScreen';
+import FeedNavigator from './FeedNavigator';
+import AccountNavigator from './AccountNavigator';
+import NewListingButton from './NewListingButton';
+import routes from './routes';
+import { PermissionsAndroid } from 'react-native';
 
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
-const AppNavigator = () => (
-	<Tab.Navigator>
-		<Tab.Screen
-			name='Feed'
-			component={FeedNavigator}
-			options={{
-				tabBarIcon: ({ color, size }) => (
-					<MaterialCommunityIcons name='home' color={color} size={size} />
-				),
-			}}
-		/>
+const AppNavigator = () => {
+	useEffect(() => {
+		registerForPushNotifications();
+	}, []);
 
-		<Tab.Screen
-			name='ListingEdit'
-			component={ListingEditScreen}
-			options={({ navigation }) => ({
-				tabBarButton: () => (
-					<NewListingButton
-						onPress={() => navigation.navigate(routes.LISTING_EDIT)}
-					/>
-				),
-				tabBarIcon: ({ color, size }) => (
-					<MaterialCommunityIcons
-						name='plus-circle'
-						color={color}
-						size={size}
-					/>
-				),
-			})}
-		/>
-		<Tab.Screen
-			name='Account'
-			component={AccountNavigator}
-			options={{
-				tabBarIcon: ({ color, size }) => (
-					<MaterialCommunityIcons
-						name='account'
-						color={color}
-						size={size}
-					/>
-				),
-			}}
-		/>
-	</Tab.Navigator>
-)
+	const registerForPushNotifications = async () => {
+		try {
+			const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+			if (!permission.granted) return;
 
-export default AppNavigator
+			const token = await Notifications.getExpoPushTokenAsync();
+			console.log(token);
+		} catch (error) {
+			console.log('Error getting a push token', error);
+		}
+	};
+
+	return (
+		<Tab.Navigator>
+			<Tab.Screen
+				name='Feed'
+				component={FeedNavigator}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<MaterialCommunityIcons name='home' color={color} size={size} />
+					),
+				}}
+			/>
+
+			<Tab.Screen
+				name='ListingEdit'
+				component={ListingEditScreen}
+				options={({ navigation }) => ({
+					tabBarButton: () => (
+						<NewListingButton
+							onPress={() => navigation.navigate(routes.LISTING_EDIT)}
+						/>
+					),
+					tabBarIcon: ({ color, size }) => (
+						<MaterialCommunityIcons
+							name='plus-circle'
+							color={color}
+							size={size}
+						/>
+					),
+				})}
+			/>
+			<Tab.Screen
+				name='Account'
+				component={AccountNavigator}
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<MaterialCommunityIcons name='account' color={color} size={size} />
+					),
+				}}
+			/>
+		</Tab.Navigator>
+	);
+};
+
+export default AppNavigator;
