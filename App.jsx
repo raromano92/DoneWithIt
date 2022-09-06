aa548b8e5bf157652ac129e35fc4d473362c5062
Robert Romano <bromano1992@gmail.com>
2022-09-05 20:08:45 -0400

    Replaced default splash screen with app branded one

--- App.jsx
+++ App.jsx
@@ -8,9 +8,11 @@ import OfflineNotice from './app/components/OfflineNotice';
 import AuthNavigator from './app/navigation/AuthNavigator';
 import AuthContext from './app/auth/context';
 import authStorage from './app/auth/storage';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
 	const [user, setUser] = useState();
+	const [isReady, setIsReady] = useState(false);
 
 	const restoreToken = async () => {
 		const token = await authStorage.getToken();
@@ -18,9 +20,14 @@ export default function App() {
 		setUser(jwtDecode(token));
 	};
 
-	useEffect(() => {
-		restoreToken();
-	}, []);
+	if (!isReady)
+		return (
+			<AppLoading
+				startAsync={restoreToken}
+				onFinish={() => setIsReady(true)}
+				onError={console.warn}
+			/>
+		);
 
 	return (
 		<AuthContext.Provider value={{ user, setUser }}>
