f8b97adc42604860d76f40e000736624e8377f58
Robert Romano <bromano1992@gmail.com>
2022-09-06 00:37:39 -0400

    Added an overlay for when app is loading

--- app/components/AppTextInput.jsx
+++ app/components/AppTextInput.jsx
@@ -1,10 +1,10 @@
-import React from "react"
-import { View, StyleSheet, TextInput } from "react-native"
-import { MaterialCommunityIcons } from "@expo/vector-icons"
+import React from 'react';
+import { View, StyleSheet, TextInput } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-import defaultStyles from "../config/styles"
+import defaultStyles from '../config/styles';
 
-export default function AppTextInput({ icon, width = "100%", ...otherProps }) {
+export default function AppTextInput({ icon, width = '100%', ...otherProps }) {
 	return (
 		<View style={[styles.container, { width }]}>
 			{icon && (
@@ -16,20 +16,20 @@ export default function AppTextInput({ icon, width = "100%", ...otherProps }) {
 				/>
 			)}
 			<TextInput
-				underlineColorAndroid="transparent"
+				underlineColorAndroid='transparent'
 				placeholderTextColor={defaultStyles.colors.medium}
 				style={defaultStyles.text}
 				{...otherProps}
 			/>
 		</View>
-	)
+	);
 }
 
 const styles = StyleSheet.create({
 	container: {
 		backgroundColor: defaultStyles.colors.light,
 		borderRadius: 25,
-		flexDirection: "row",
+		flexDirection: 'row',
 		padding: 15,
 		marginVertical: 10,
 	},
@@ -37,4 +37,4 @@ const styles = StyleSheet.create({
 		marginRight: 10,
 		marginTop: 5,
 	},
-})
+});
