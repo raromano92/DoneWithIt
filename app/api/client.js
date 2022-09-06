892d20e8aaeeca0984802cc730a526b00b8c2fcd
Robert Romano <bromano1992@gmail.com>
2022-09-05 23:22:17 -0400

    Added registration api function for new users

--- app/api/client.js
+++ app/api/client.js
@@ -1,13 +1,17 @@
 import { create } from 'apisauce';
 import cache from '../utility/cache';
+import authStorage from '../auth/storage';
 
 const apiClient = create({
 	baseURL: 'http://10.0.0.18:9000/api/',
 });
 
+apiClient.addAsyncRequestTransform(async (request) => {
+	const authToken = await authStorage.getToken();
+});
+
 const get = apiClient.get;
 apiClient.get = async (url, params, axiosConfig) => {
-	// Before
 	const response = await get(url, params, axiosConfig);
 
 	if (response.ok) {
