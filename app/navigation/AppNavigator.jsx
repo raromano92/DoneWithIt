a6f0fe90000ae1430c92e53b9c47c7d92c16cc91
Robert Romano <bromano1992@gmail.com>
2022-09-06 12:17:20 -0400

    Added custom hook for push notifications

--- app/navigation/AppNavigator.jsx
+++ app/navigation/AppNavigator.jsx
@@ -1,32 +1,49 @@
 import React, { useEffect } from 'react';
+import { Platform } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import * as Notifications from 'expo-notifications';
-import * as Permissions from 'expo-permissions';
+import * as Device from 'expo-device';
 
 import ListingEditScreen from '../screens/ListingEditScreen';
 import FeedNavigator from './FeedNavigator';
 import AccountNavigator from './AccountNavigator';
 import NewListingButton from './NewListingButton';
 import routes from './routes';
-import { PermissionsAndroid } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
 	useEffect(() => {
-		registerForPushNotifications();
+		registerForPushNotificationsAsync();
 	}, []);
 
-	const registerForPushNotifications = async () => {
-		try {
-			const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-			if (!permission.granted) return;
-
-			const token = await Notifications.getExpoPushTokenAsync();
+	const registerForPushNotificationsAsync = async () => {
+		if (Device.isDevice) {
+			const { status: existingStatus } = await Notifications.getPermissionsAsync();
+			let finalStatus = existingStatus;
+			if (existingStatus !== 'granted') {
+				const { status } = await Notifications.requestPermissionsAsync();
+				finalStatus = status;
+			}
+			if (finalStatus !== 'granted') {
+				alert('Failed to get push token for push notification!');
+				return;
+			}
+			const token = (await Notifications.getExpoPushTokenAsync()).data;
 			console.log(token);
-		} catch (error) {
-			console.log('Error getting a push token', error);
+			this.setState({ expoPushToken: token });
+		} else {
+			alert('Must use physical device for Push Notifications');
+		}
+
+		if (Platform.OS === 'android') {
+			Notifications.setNotificationChannelAsync('default', {
+				name: 'default',
+				importance: Notifications.AndroidImportance.MAX,
+				vibrationPattern: [0, 250, 250, 250],
+				lightColor: '#FF231F7C',
+			});
 		}
 	};
 
